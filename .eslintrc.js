d25fb57c4f4ed532ce20c2ff88358353c934a2b9
PARKER\Parker <parkers@mail.ru>
2021-11-05 17:30:36 +0300

    Add jest to project

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,10 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "jest/globals": true,
   },
   extends: ["airbnb-base", "prettier"],
+  plugins: ["jest"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
